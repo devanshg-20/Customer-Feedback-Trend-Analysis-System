5acff3cbebaf05172bc19f8288b9cd970bef402b
DEVANSH GUPTA <gdevansh755@gmail.com>
2026-04-08 19:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -717,3 +717,7 @@ def voice():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
